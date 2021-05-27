a2a553f13c53743ff644c1432a51afb63716ec7b
Pedro Raimondi <pedroksk8@hotmail.com>
2021-05-27 08:31:53 -0300

    fix booksError

--- pages/livros/[name]/Book.js
+++ pages/livros/[name]/Book.js
@@ -7,6 +7,6 @@ export default function Book({ hasAudiovisual, ...props }){
   const books = props.books ? JSON.parse(props.books) : [];
   if(book.audio && hasAudiovisual){
     return <BookAudiovisual {...props} book={book}/>
-  } else if (book && books && book.audio) { return <BookComponent {...props} book={book} books={books} /> }
+  } else if (book && books) { return <BookComponent {...props} book={book} books={books} /> }
   return <BlankPage />
 }
