4296c89e2fd8ea5cdc00bb066f7fa241078fbfaf
Pedro Raimondi <pedroksk8@hotmail.com>
2021-06-01 10:38:27 -0300

    fix user list

--- pages/autores/index.js
+++ pages/autores/index.js
@@ -4,7 +4,8 @@ import User from '../../models/user';
 export async function getServerSideProps() {
   await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URL, { useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true });
   const usersArray = await User.find();
-  const casts = usersArray ? JSON.stringify(usersArray.filter((item) => !!item?.occupation?.length && item.occupation?.some((occupation) => ['illustrator', 'writer'].includes(occupation)))) : []
+  const castsArray = await User.find({ hideFromList: { $ne: true } });
+  const casts = usersArray ? JSON.stringify(castsArray.filter((item) => !!item?.occupation?.length && item.occupation?.some((occupation) => ['illustrator', 'writer'].includes(occupation)))) : []
   const users = usersArray ? JSON.stringify(usersArray) : {}
   return { props: { users, casts } }
 }
