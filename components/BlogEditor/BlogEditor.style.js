a3ff5e3bb4c5561a7ca611745f6203985933e5e2
tiagokise <tiago.kise@hotmail.com>
2021-07-18 14:00:41 -0300

    styling blog

--- components/BlogEditor/BlogEditor.style.js
+++ components/BlogEditor/BlogEditor.style.js
@@ -3,9 +3,9 @@ import Container from '../Container'
 
 export const BlogEditor = styled(Container)`
   .rdw-editor-toolbar{
-    padding: 6px;
+    padding: 6px 6px 0;
     border-radius: 8px 8px 0 0;
-    background: lightslategrey;
+    background: #00A79D;
     margin-bottom: 0px;
   }
   .rdw-option-wrapper {
@@ -19,6 +19,23 @@ export const BlogEditor = styled(Container)`
   .public-DraftStyleDefault-block {
     margin: 1em;
   }
+  .rdw-dropdown-wrapper {
+    height: 26px;
+    border-radius: 6px;
+
+  }
+  .rdw-dropdown-optionwrapper{
+    border-radius: 6px;
+    padding: 4px;
+    -ms-overflow-style: none; 
+    scrollbar-width: none; 
+    overflow: auto;
+    box-shadow: 3px 3px 2px rgb(115 137 169 / 20%);
+    background: #f2f2f2;
+  }
+  .rdw-fontfamily-optionwrapper {
+    width: max-content;
+}
 
 `
 
