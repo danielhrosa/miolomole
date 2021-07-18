c69018259bde8621796a031352c8c2bc1560536f
tiagokise <tiago.kise@hotmail.com>
2021-07-18 12:21:31 -0300

    styling editor

--- components/BlogEditor/BlogEditor.js
+++ components/BlogEditor/BlogEditor.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import Slider from 'react-slick';
+import { useAppProvider } from '../../store/appProvider';
 import * as S from './BlogEditor.style'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 export default function BlogEditor(){
   const [editorState, onEditorStateChange] = useState(EditorState.createEmpty(''));
-
+  const { isLoggedIn } = useAppProvider();
+  console.log(isLoggedIn);
 
   return (
-    <S.BlogEditor>
-      <Editor editorState={editorState} onEditorStateChange={onEditorStateChange}/>
+    <S.BlogEditor isLoggedIn={isLoggedIn}>
+      <Editor 
+        toolbarHidden={!isLoggedIn} 
+        readOnly={!isLoggedIn} 
+        editorState={editorState} 
+        onEditorStateChange={onEditorStateChange}
+      />
     </S.BlogEditor>
   )
 }
--- components/BlogEditor/BlogEditor.style.js
+++ components/BlogEditor/BlogEditor.style.js
@@ -2,6 +2,23 @@ import styled from 'styled-components';
 import Container from '../Container'
 
 export const BlogEditor = styled(Container)`
+  .rdw-editor-toolbar{
+    padding: 6px;
+    border-radius: 8px 8px 0 0;
+    background: lightslategrey;
+    margin-bottom: 0px;
+  }
+  .rdw-option-wrapper {
+    padding: 12px;
+    border-radius: 6px;
+  }
+  .rdw-editor-main{
+    background: ${({isLoggedIn}) => isLoggedIn ? '#f2f2f2' : 'white'};
+    border-radius: 0 0 8px 8px;
+  }
+  .public-DraftStyleDefault-block {
+    margin: 1em;
+  }
 
 `
 
