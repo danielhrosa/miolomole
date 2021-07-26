c9ec3e34f08cc10902c8ea390ea05a0a32124b8c
Pedro Raimondi <pedroksk8@hotmail.com>
2021-07-26 17:49:03 -0300

    fix redirect fail to ralf guy

--- pages/livros/[name]/[assetType]/index.js
+++ pages/livros/[name]/[assetType]/index.js
@@ -4,7 +4,7 @@ import Book from '../../../../models/book';
 export async function getServerSideProps({ params }) {
   await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URL, { useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true });
   try { 
-    const book = await Book.findOne({ name: params.name });
+    const book = await Book.findOne({ name: new RegExp(params.name, "g") });
     return { props: { book: JSON.stringify(book), params } }
   } catch { return }; 
 }
--- pages/livros/[name]/index.js
+++ pages/livros/[name]/index.js
@@ -4,30 +4,12 @@ import Text from '../../../models/text';
 
 export async function getServerSideProps({ params: { name } }) {
   await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URL, { useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true });
-  const hasExtraContentsInURL = [
-    'verao-audiovisual',
-    'versao-ausiovisual',
-    'buyua-wasu-versao-audiovisual',
-    'versao-audiovisual-acessivel',
-    'versao-audiovisual',
-    'audiovisual',
-    'versao-audioacessivel',
-  ]
-  let partURL, bookName, booksSearch;
-  let book = null;
-  let hasAudiovisual = null;
-  const page = 'book';
-  const textsArray = await Text.find({ page });
+  const booksArr = await Book.find().populate('authors').populate('illustrators');
+  const book = JSON.stringify(booksArr.find((item) => item.name.includes(name)));
+  const books = JSON.stringify(booksArr);
+  const textsArray = await Text.find({ page: 'book' });
   const texts = textsArray.reduce((object, text) => Object.assign(object, {[text.textKey]: text.text}), {});
-  if (hasExtraContentsInURL.some((part) => { if(name.includes(part)) { partURL = part; return true } })) { 
-    hasAudiovisual = true; 
-  }
-  const booksArr = await Book.find();
-  const books = booksArr ? JSON.stringify(booksArr) : [];
-  bookName = name.replace(`-${partURL}`, '')
-  if(bookName) { booksSearch = await Book.find({ name: new RegExp(bookName, "g") }).populate('authors').populate('illustrators') };
-  if(booksSearch.length) { book = JSON.stringify(booksSearch[0]) };
-  return { props: { book, hasAudiovisual, texts, books, page } }
+  return { props: { book, books, texts, page: 'book' } }
 }
 
 export { default } from './Book';
