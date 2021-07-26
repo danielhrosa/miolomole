f6909deaa56e89212544c245124030ce8529ad56
Gustavo Garcia <gustavo.ogarcia@gmail.com>
2021-07-25 23:14:11 -0300

    toCreck

---
+++ pages/livros/[name]/[assetType]/index.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import Book from '../../../../models/book';
+
+export async function getServerSideProps({ params }) {
+  await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URL, { useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true });
+  try { 
+    const book = await Book.findOne({ name: params.name });
+    return { props: { book: JSON.stringify(book), params } }
+  } catch { return }; 
+}
+
+export { default } from '../../../../components/Assets';
--- pages/livros/index.js
+++ pages/livros/index.js
@@ -1,15 +1,22 @@
 import mongoose from 'mongoose';
-import Book from '../../models/book';
 import Text from '../../models/text';
+import Book from '../../models/book';
+import Highlight from '../../models/highlight';
+import User from '../../models/user';
 
-export async function getServerSideProps({ params: { name } }) {
+export async function getServerSideProps() {
   await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URL, { useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true });
-  const booksArr = await Book.find().populate('authors').populate('illustrators');
-  const book = JSON.stringify(booksArr.find((item) => name === item.name));
-  const books = JSON.stringify(booksArr);
-  const textsArray = await Text.find({ page: 'book' });
+  const page = 'books';
+  const textsArray = await Text.find({ page });
+  const booksArray = await Book.find();
+  const highlightsArray = await Highlight.find({ isActive: true });
+  const highlights = !!highlightsArray.length ? JSON.stringify(highlightsArray) : '[]';
+  let itemsArray = await User.find({ hideFromList: { $ne: true } });
+  itemsArray = itemsArray.filter((item) => !!item?.occupation?.length && item.occupation?.some((occupation) => ['illustrator', 'writer'].includes(occupation)))
+  const items = itemsArray ? JSON.stringify(itemsArray) : {}
+  const books = JSON.stringify(booksArray);
   const texts = textsArray.reduce((object, text) => Object.assign(object, {[text.textKey]: text.text}), {});
-  return { props: { book, books, texts, page: 'book' } }
+  return { props: { texts, books, items, highlights, page } }
 }
 
 export { default } from './Books';
