c33a2a55615aaa6fa6ffd60d95f521bcbb88d5ff
Pedro <pedro@grupoeureka.com>
2021-10-06 14:35:03 -0300

    feat: removendo loja do foter

--- components/FooterNav/FooterNav.js
+++ components/FooterNav/FooterNav.js
@@ -8,7 +8,6 @@ export default function FooterNav(){
   const navMenuItems = [
     {name: 'Home', path: '/'},
     {name: 'Livros', path: '/livros' },
-    {name: 'Loja', path: '/loja'},
     {name: 'Blog', path: '/blog'},
     {name: 'Quem somos', path: '/quem-somos'},
     {name: 'Contato', path: '/contato'},
