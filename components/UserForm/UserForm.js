ea91162cf7658fdaf586266e661a8e15d5319264
Pedro Raimondi <vte_psilva@uolinc.com>
2022-07-12 23:34:06 -0300

    feat: add loading to userForm button

--- components/UserForm/UserForm.js
+++ components/UserForm/UserForm.js
@@ -9,25 +9,26 @@ import mapFieldsToData from '../../utils/mapFieldsToData';
 import mapDataToFields from '../../utils/mapDataToFields';
 import { usuariosFieldsState, usuariosFieldsFunction, gridTemplate } from './UserForm.constants.js';
 
-export default function UserForm(props){
+export default function UserForm(props) {
   const router = useRouter();
   const [user, setUser] = useState();
   const [fields, setFields] = useState(usuariosFieldsState);
-  const userFormfields = usuariosFieldsFunction({ fields });
+  const [loading, setLoading] = useState(false);
+  const userFormfields = usuariosFieldsFunction({ fields, setFields });
   const formProps = { gridTemplate, fields: userFormfields, setFields };
 
   useEffect(() => {
-    if(props.user) {
+    if (props.user) {
       setUser(props.user)
       setFields((oldFields) => {
-        const newFields = {...oldFields};
+        const newFields = { ...oldFields };
         mapDataToFields({ newFields, constantFields: userFormfields, data: props.user })
         return newFields
       })
     }
   }, [props.user])
 
-  if(
+  if (
     userFormfields?.occupation?.value.length === 0 ||
     userFormfields?.occupation?.value.length && !userFormfields?.occupation?.value.some((occupation) => occupation?.value === 'admin') ||
     !!userFormfields?.occupation?.value?.value && !userFormfields?.occupation?.value?.value.some((occupation) => occupation === 'admin')
@@ -36,23 +37,25 @@ export default function UserForm(props){
   }
 
   const onSubmit = async () => {
+    setLoading(true);
     let variables = mapFieldsToData(userFormfields);
-    if(variables.password === ''){ delete variables.password }
+    if (variables.password === '') { delete variables.password }
     let res;
-    try{
-      if(!user) {res = await axios.post('/api/users', { ...variables })}
-      else {res = await axios.put('/api/users', { ...variables, _id: user._id })}
-      if(res.status === 200) { toast.success(`Cadastrado ${user ? 'atualizado' : 'realizado'} com sucesso!`) }
+    try {
+      if (!user) { res = await axios.post('/api/users', { ...variables }); }
+      else { res = await axios.put('/api/users', { ...variables, _id: user._id }) }
+      if (res.status === 200) { toast.success(`Cadastrado ${user ? 'atualizado' : 'realizado'} com sucesso!`) }
       else { toast.error(res?.response?.data) }
     } catch (err) { toast.error(err.response.data.errorMessage) }
+    setLoading(false);
   }
-  return(
+  return (
     <S.UsuariosWrapper>
       <Button variation="primary" onClick={() => router.push('/autores')}>Voltar</Button>
       <h1>Adicionar usuário</h1>
       <Form {...formProps} />
-      <Toaster position="bottom-right" reverseOrder={false}/>
-      <Button variation="primary" label={user ? "Atualizar" : "Cadastrar"} onClick={() => onSubmit()}/>
+      <Toaster position="bottom-right" reverseOrder={false} />
+      <Button loading={loading} variation="primary" label={user ? "Atualizar" : "Cadastrar"} onClick={() => onSubmit()} />
     </S.UsuariosWrapper>
   )
 }
