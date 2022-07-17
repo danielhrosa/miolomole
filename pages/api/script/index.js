3c61e4fa7a0fb602ead09cccd2777b1aa56f3ec2
Pedro Raimondi <vte_psilva@uolinc.com>
2022-07-17 02:20:02 -0300

    feat: endpoint for dev scripts

---
+++ pages/api/script/index.js
@@ -0,0 +1,17 @@
+import connectDB from '../../../middleware/mongodb';
+import Book from '../../../models/book';
+import updateModel from '../../../utils/updateModel';
+
+const bookHandler = async (req, res) => {
+  try {
+    const books = await Book.find().lean();
+    for (const book of books) {
+      const updatedModel = await updateModel({ ...book, digitalExperiencePrice: '-' }, Book);
+      updatedModel.save();
+    }
+  } catch (e) {
+    return res.status(400).json({ response: `Error: ${e}` }).end();
+  }
+  return res.status(200).json({ response: "Executed" });
+}
+export default connectDB(bookHandler);
