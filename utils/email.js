6af0ff08857d76447e3c9e28886c04bedeb739d1
Pedro Raimondi <vte_psilva@uolinc.com>
2022-07-27 01:22:49 -0300

    fix: test email in production

--- utils/email.js
+++ utils/email.js
@@ -3,9 +3,10 @@ import nodemailer from "nodemailer";
 const host = process.env.EMAIL_HOST;
 const port = Number(process.env.EMAIL_PORT);
 const email = process.env.EMAIL;
+const pass = process.env.PASSWORD;
 
 export const createEmail = ({ to, html = "", subject = "" }) => {
   return { from: `"Miolo Mole" <${email}>`, to, subject, html };
 };
 
-export const transport = nodemailer.createTransport({ host, port, auth: { user: email, pass: process.env.PASSWORD } });
+export const transport = nodemailer.createTransport({ host, port, auth: { user: email, pass } });
