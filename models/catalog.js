7745569190a81216f8ed9bcd0fc21b6ce075c0ca
Pedro Raimondi <vte_psilva@uolinc.com>
2022-08-23 03:17:20 -0300

    feat: catalog model

---
+++ models/catalog.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+var Schema = mongoose.Schema;
+
+var catalog = new Schema({
+  color: { type: String },
+  label: { type: String, required: true },
+  link: { type: String, required: true },
+}, { timestamps: true });
+
+mongoose.models = {};
+
+var Catalog = mongoose.model('Catalog', catalog);
+
+export default Catalog;
