a5277f28ce58fd73d8b749ff1e56ff807c5c62d9
Pedro Raimondi <vte_psilva@uolinc.com>
2022-08-23 22:43:47 -0300

    logo

--- components/Logo/Logo.js
+++ components/Logo/Logo.js
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import { useTheme } from 'styled-components';
 import LogoMiolo from '../../images/js/Logo-miolo'
 
 export default function Logo() {
+  const theme = useTheme();
   return (
     <Link href={'/'}>
-      <a><LogoMiolo /></a>
+      <a><LogoMiolo color={theme.color.brand} /></a>
     </Link>
   )
 }
