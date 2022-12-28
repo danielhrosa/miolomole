af33ef431590a1d08cb1bbf3391bcb29007316a3
Pedro Raimondi <vte_psilva@uolinc.com>
2022-12-28 13:44:00 -0300

    improve editar-menu security

--- pages/editar-menu/index.js
+++ pages/editar-menu/index.js
@@ -3,10 +3,16 @@ import PageJustForAdmin from '../../components/PageJustForAdmin'
 import EditMenuPage from '../../components/EditMenuPage/EditMenuPage'
 import Pages from '../../models/pages'
 import { useAppProvider } from '../../store/appProvider';
+import { getCookies } from 'cookies-next';
+import jwt from 'jsonwebtoken';
 
 export async function getServerSideProps({ req, res }) {
   mongoose.connect(process.env.NEXT_PUBLIC_MONGO_DB_URL, { useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true, useNewUrlParser: true });
-  const pagesArray = await Pages.find({ path: { $ne: '/editar-menu' } });
+
+  const { TK } = getCookies({ req, res });
+  const { _id: token } = jwt.decode(TK, process.env.SECRET_KEY) || { token: undefined };
+
+  const pagesArray = await Pages.find(token ? { path: { $ne: '/editar-menu' } } : { isPrivate: { $ne: true }, path: { $ne: '/editar-menu' } });
   const pages = !!pagesArray?.length ? JSON.stringify(pagesArray) : `[]`;
 
   return { props: { pages } }
