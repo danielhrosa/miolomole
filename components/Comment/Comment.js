c580baa8aeedc464ad4488179d55c629b6be91aa
Pedro Raimondi <pedroksk8@hotmail.com>
2023-06-26 00:40:31 -0300

    changing to notebook

--- components/Comment/Comment.js
+++ components/Comment/Comment.js
@@ -1,8 +1,18 @@
 import * as S from './Comment.styles';
+import { useAppProvider } from '../../store/appProvider';
 
 export default function Comment(comment) {
+  const { isLoggedIn } = useAppProvider()
   return (
     <S.Comment key={comment._id}>
+      {isLoggedIn && (
+        <S.EditableButtons>
+          <S.EditButton onClick={() => edit ? saveText() : setEdit(true)}>
+            {edit ? <Button id={`${textKey}ConfirmButton`} type="confirm" /> : <Button id={`${textKey}EditButton`} type="edit" />}
+          </S.EditButton>
+          {edit && <Button id={`${textKey}CancelButton`} onClick={() => { setNewText(text); setEdit(false) }} type="cancel" />}
+        </S.EditableButtons>
+      )}
       <S.UserFullName>{comment?.userFullName}</S.UserFullName>
       <S.Content>{comment?.content}</S.Content>
     </S.Comment>
