44017e7a1d28baf444f0b054f2fdcc786c8ca7f8
Pedro Raimondi <pedroksk8@hotmail.com>
2023-11-20 13:01:58 -0300

    change label

--- components/PDFReader/PDFReader.js
+++ components/PDFReader/PDFReader.js
@@ -8,10 +8,10 @@ export default function PDFReader(props) {
 
   return (
     <S.PDFReaderWrapper>
-      <Button variation="primary" onClick={() => { setIsFullScrenOn(!isFullScrenOn) }} label="Ler em tela cheia" />
+      <Button variation="primary" onClick={() => { setIsFullScrenOn(!isFullScrenOn) }} label="Acessar obra" />
       <S.PDFReader src={props?.src + '#toolbar=0'} />
       {isFullScrenOn && <S.PDFReaderFullView>
-        <S.ButtonClose variation="close" onClick={() => { setIsFullScrenOn(!isFullScrenOn) }} label="Sair da tela cheia" /> 
+        <S.ButtonClose variation="close" onClick={() => { setIsFullScrenOn(!isFullScrenOn) }} label="Fechar obra" /> 
         <S.PDFReader src={props?.src + '#toolbar=0'} isFullScrenOn={isFullScrenOn} />
       </S.PDFReaderFullView>}
     </S.PDFReaderWrapper>
