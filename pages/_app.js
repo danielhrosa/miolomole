f95cbfb45770cf829e80e22a3177e93dc69e728c
Pedro Luis Raimondi Ferreira da Silva <pedroraimondi6@gmail.com>
2024-02-02 05:27:57 -0300

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+      `}
+      </Script>
       <Head>
         <title>Miolo Mole</title>
       </Head>
