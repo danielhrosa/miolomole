f728bca6fb588fbf617f86a0a0e472151168b3f9
Pedro Luis Raimondi Ferreira da Silva <pedroraimondi6@gmail.com>
2024-03-28 05:26:37 -0300

    Blog text

--- components/EducatorAreaPublication/EducatorAreaPublication.styles.js
+++ components/EducatorAreaPublication/EducatorAreaPublication.styles.js
@@ -3,7 +3,31 @@ import styled from "styled-components";
 export const EducatorAreaPublication = styled.div`
   width: 100%;
   padding-top: 70px;
-  .sun-editor { border: none; }
+  .sun-editor { 
+    border: none;
+    margin-bottom: 32px;
+  }
+
+  .sun-editor-editable:after {
+    content: "";
+    width: 100%;
+    position: absolute;
+    border-bottom: 5px dashed #30303030;
+    bottom: 0;
+    left: 0;
+  }
+
+  .sun-editor-editable p {
+    width: 70vw;
+    text-align: justify;
+    line-height: 2.2;
+    margin: 0 auto 30px !important;
+    
+    @media (min-width: 768px) {
+      width: 600px;
+    }
+  }
+
   .se-resizing-bar { display: none !important; }
 `;
 
