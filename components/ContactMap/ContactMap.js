086d90338f3565536e05f2a15f93c7ce9302e127
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2025-04-25 11:53:46 -0300

    Update ContactMap.js

--- components/ContactMap/ContactMap.js
+++ components/ContactMap/ContactMap.js
@@ -4,7 +4,7 @@ export default function ContactMap(){
 
  return(
     <S.ContactMap>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.130746905933!2d-46.69535769999999!3d-23.5277994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5b2e702af177%3A0xc1f8096ac024a640!2sMiolo%20Mole%20Editora!5e0!3m2!1spt-BR!2sbr!4v1745591793277!5m2!1spt-BR!2sbr"></iframe>
+           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.130746905933!2d-46.69535769999999!3d-23.5277994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5b2e702af177%3A0xc1f8096ac024a640!2sMiolo%20Mole%20Editora!5e0!3m2!1spt-BR!2sbr!4v1745591793277!5m2!1spt-BR!2sbr"loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </S.ContactMap>
   )
 }
