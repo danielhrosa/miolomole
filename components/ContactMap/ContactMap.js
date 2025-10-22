3786b80808c0369618f66f8c284a531418b47ea5
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2025-10-22 17:31:30 -0300

    Update ContactMap.js

--- components/ContactMap/ContactMap.js
+++ components/ContactMap/ContactMap.js
@@ -1,2 +1,10 @@
 import * as S from './ContactMap.style.js'
 
+export default function ContactMap(){
+
+ return(
+    <S.ContactMap>
+           <iframe src=""></iframe>
+    </S.ContactMap>
+  )
+}
