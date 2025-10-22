24ac04b6292f09c040c4b046b79006e2054b1bf4
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2025-10-22 18:05:11 -0300

    Update Contact.js

--- pages/contato/Contact.js
+++ pages/contato/Contact.js
@@ -1,1 +1,14 @@
+import ContactMap from "../../components/ContactMap";
+import ContactSection from "../../components/ContactSection";
+
+export default function Contact(props) {
+  
+  return (
+    <div>
+      <ContactMap />
+      <ContactSection {...props} />
+    </div>
+  )
+
+}
 
