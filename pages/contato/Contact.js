95387065e392200912cdb9450e41b60647823828
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2025-10-22 17:28:28 -0300

    Cancelando mapa

--- pages/contato/Contact.js
+++ pages/contato/Contact.js
@@ -4,10 +4,7 @@ import ContactSection from "../../components/ContactSection";
 export default function Contact(props) {
   
   return (
-    <div>
-      <ContactMap />
-      <ContactSection {...props} />
-    </div>
+
   )
 
 }
