0b04077dbfcb1809b97e3a296b52f0d4dd7912da
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2025-10-22 17:33:32 -0300

    mapa

--- pages/contato/Contact.js
+++ pages/contato/Contact.js
@@ -4,7 +4,10 @@ import ContactSection from "../../components/ContactSection";
 export default function Contact(props) {
   
   return (
-
+    <div>
+      <ContactMap />
+      <ContactSection {...props} />
+    </div>
   )
 
 }
