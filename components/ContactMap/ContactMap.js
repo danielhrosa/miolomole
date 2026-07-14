ad5f3b6d795bd459ba5c96bf5d162803a387047d
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2026-07-14 15:38:44 -0300

    Update ContactMap.js

--- components/ContactMap/ContactMap.js
+++ components/ContactMap/ContactMap.js
@@ -4,7 +4,7 @@ export default function ContactMap() {
   return (
     <S.ContactMap>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7316.38272696423!2d-46.6900684!3d-23.525618699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5b2e702af177%3A0xc1f8096ac024a640!2sMiolo%20Mole%20Editora!5e0!3m2!1spt-BR!2sbr!4v1784053055581!5m2!1spt-BR!2sbr"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7316.261710636703!2d-46.6953573!3d-23.5277955!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef876972e06ef%3A0x1e53964ab2a69105!2sR.%20Vespasiano%2C%20581%20-%20Vila%20Romana%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2005044-050!5e0!3m2!1spt-BR!2sbr!4v1784054253620!5m2!1spt-BR!2sbr"
         width="100%"
         height="300"
         style={{ border: 0 }}
