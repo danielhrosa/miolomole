16d1313f88b64038f0c46cf602b0557ec0464b92
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2026-07-14 15:21:52 -0300

    Update ContactMap.style.js

--- components/ContactMap/ContactMap.style.js
+++ components/ContactMap/ContactMap.style.js
@@ -1,8 +1,19 @@
 import styled from 'styled-components';
 
 export const ContactMap = styled.div`
-  iframe{
-    width: 0%;
-    height: 0px;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+
+  iframe {
+    width: 100%;
+    height: 300px;
+    border-radius: 8px;
+  }
+
+  @media (min-width: 768px) {
+    iframe {
+      height: 400px;
+    }
   }
 `;
