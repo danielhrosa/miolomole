7c3f20316ea9d9274daf4b0b45dad149130f9476
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2026-07-15 19:54:09 -0300

    Update ContactAddress.js

--- components/ContactAddress/ContactAddress.js
+++ components/ContactAddress/ContactAddress.js
@@ -10,7 +10,7 @@ export default function ContactChannel(){
     <S.ContactAddressListItem className="contactAddress" name="address">
       <a href="https://maps.app.goo.gl/wRpQnLLa8HcjAMee8" target="_blank">
         <Icon type={t.ADDRESS.ICON}/>
-        <S.Label><p>Praça Cornélia, 89 - Água Branca, São Paulo - SP, 05043-030</p></S.Label>
+        <S.Label>'Praça Cornélia, 89 - Água Branca, São Paulo - SP, 05043-030'</S.Label>
       </a>
     </S.ContactAddressListItem>
   )
