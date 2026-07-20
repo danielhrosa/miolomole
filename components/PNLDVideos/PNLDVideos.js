0823b4f1915aa9e28839dc3b9a88d4dd86bdac8b
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2026-07-20 13:29:31 -0300

    Update PNLDVideos.js

--- components/PNLDVideos/PNLDVideos.js
+++ components/PNLDVideos/PNLDVideos.js
@@ -59,18 +59,16 @@ export default function PNLDVideos({ pnld, book, ...props }) {
         <S.PNLDVideosWrapper>
           <S.PNLDOurWorksBookTitle>Vídeos</S.PNLDOurWorksBookTitle>
           <S.PNLDVideoPlayer>
-            <S.PNLDOurWorksBookTitle>{/* Vídeo do estudante */}</S.PNLDOurWorksBookTitle>
+            <S.PNLDOurWorksBookTitle>Vídeo do estudante</S.PNLDOurWorksBookTitle>
             <Input {...videosFields.pnldVideoStudent} {...inputProps} />
           </S.PNLDVideoPlayer>
-  
-      <S.Dotted />  
+          <S.Dotted />
           <S.PNLDVideoPlayer>
-            <S.PNLDOurWorksBookTitle>{/* Vídeo do professor */}</S.PNLDOurWorksBookTitle>
+            <S.PNLDOurWorksBookTitle>Vídeo do professor</S.PNLDOurWorksBookTitle>
             {(!videosFields.pnldVideoTeacher?.value && !isLoggedIn) ? (
-              <S.PNLDOurWorksBookTitle>{/* Sem vídeo cadastrado */}</S.PNLDOurWorksBookTitle>
+              <S.PNLDOurWorksBookTitle>Sem vídeo cadastrado</S.PNLDOurWorksBookTitle>
             ) : <Input {...videosFields.pnldVideoTeacher} {...inputProps} />}
           </S.PNLDVideoPlayer>
-  
         </S.PNLDVideosWrapper>
         {isLoggedIn && <Button label="Salvar videos" variation="primary" onClick={saveVideos} />}
         <BookRelated pnld={pnld.name} textKey="PNLDOtherWorks" {...props} />
