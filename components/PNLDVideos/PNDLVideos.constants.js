e1840dfc9d4b327fe17ede3bc4eb6ed37909dfb7
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2026-07-20 14:15:08 -0300

    Update PNDLVideos.constants.js

--- components/PNLDVideos/PNDLVideos.constants.js
+++ components/PNLDVideos/PNDLVideos.constants.js
@@ -1,6 +1,6 @@
 export const inputFieldsState = () => ({
   pnldVideoStudent: { value: '' },
-  //pnldVideoTeacher: { value: '' },
+  pnldVideoTeacher: { value: '' },
 });
 
 export const inputFieldsFunction = ({ fields }) => ({
