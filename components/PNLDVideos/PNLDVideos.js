d1d6d10029b677544018ba77c772c845566a6a82
Miolo Mole <79093919+danielhrosa@users.noreply.github.com>
2026-07-20 13:35:51 -0300

    Update PNLDVideos.js

--- components/PNLDVideos/PNLDVideos.js
+++ components/PNLDVideos/PNLDVideos.js
@@ -59,14 +59,14 @@ export default function PNLDVideos({ pnld, book, ...props }) {
         <S.PNLDVideosWrapper>
           <S.PNLDOurWorksBookTitle>Vídeos</S.PNLDOurWorksBookTitle>
           <S.PNLDVideoPlayer>
-            <S.PNLDOurWorksBookTitle>Vídeo do estudante</S.PNLDOurWorksBookTitle>
+            <S.PNLDOurWorksBookTitle> </S.PNLDOurWorksBookTitle>
             <Input {...videosFields.pnldVideoStudent} {...inputProps} />
           </S.PNLDVideoPlayer>
           <S.Dotted />
           <S.PNLDVideoPlayer>
-            <S.PNLDOurWorksBookTitle>Vídeo do professor</S.PNLDOurWorksBookTitle>
+            <S.PNLDOurWorksBookTitle> </S.PNLDOurWorksBookTitle>
             {(!videosFields.pnldVideoTeacher?.value && !isLoggedIn) ? (
-              <S.PNLDOurWorksBookTitle>Sem vídeo cadastrado</S.PNLDOurWorksBookTitle>
+              <S.PNLDOurWorksBookTitle> </S.PNLDOurWorksBookTitle>
             ) : <Input {...videosFields.pnldVideoTeacher} {...inputProps} />}
           </S.PNLDVideoPlayer>
         </S.PNLDVideosWrapper>
